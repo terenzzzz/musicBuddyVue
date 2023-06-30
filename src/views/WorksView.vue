f06f84f19e4d46c1f072f92959b6b63a0b41e23a
terenzzzz <terence592592@gmail.com>
2023-06-30 19:49:46 +0800

    轮播图

--- src/views/WorksView.vue
+++ src/views/WorksView.vue
@@ -1,7 +1,20 @@
 <template>
     <div>
         <TopHeader></TopHeader>
-        WorksView
+        
+        <div id="swipe-container">
+            <van-swipe :autoplay="3000" indicator-color="skyblue">
+                <van-swipe-item><img src="../assets/birdwatching.png" alt=""></van-swipe-item>
+                <van-swipe-item><img src="../assets/avatar.png" alt=""></van-swipe-item>
+                <van-swipe-item><img src="../assets/PVE.png" alt=""></van-swipe-item>
+                <van-swipe-item><img src="../assets/PVE.png" alt=""></van-swipe-item>
+            </van-swipe>
+        </div>
+        
+        
+        <SingleBlog></SingleBlog>
+        <SingleBlog></SingleBlog>
+        <SingleBlog></SingleBlog>
         <BottomFooter></BottomFooter>
     </div>
 </template>
@@ -9,15 +22,28 @@
 <script>
 import TopHeader from '@/components/TopHeader';
 import BottomFooter from '@/components/BottomFooter';
+import SingleBlog from '@/components/SingleBlog';
 
 export default {
     components: {
         TopHeader,
-        BottomFooter
-    }
+        BottomFooter,
+        SingleBlog
+    },
+    
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="css" scoped>
+#swipe-container{
+    height: 500px;
+    display: flex;
+    justify-content: center;
+}
 
+#swipe-container img{
+    width: 100%;
+    height: auto;
+
+}
 </style>
