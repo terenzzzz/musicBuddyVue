b694b26fd59761629647b86aac17eedb8df5739e
Terenzzzz <terence592592@gmail.com>
2023-09-30 00:53:42 +0100

    fix

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,8 @@
 import axios from 'axios'
 
 // 开发使用
-axios.defaults.baseURL = "http://0.0.0.0:592"
+// axios.defaults.baseURL = "http://0.0.0.0:592"
+axios.defaults.baseURL = "http://47.106.139.89:592"
 
 
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
