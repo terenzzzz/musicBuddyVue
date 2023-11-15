130424370fabd3db6558526d447af308772f055c
Terenzzzz <terence592592@gmail.com>
2023-11-15 17:49:10 +0000

    change API address to 0.0.0.0:592

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,8 +2,10 @@
 import axios from 'axios'
 
 // 开发使用
-// axios.defaults.baseURL = "http://0.0.0.0:592"
-axios.defaults.baseURL = "http://47.106.139.89:592"
+axios.defaults.baseURL = "http://0.0.0.0:592"
+
+
+// axios.defaults.baseURL = "http://47.106.139.89:592"
 
 
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
