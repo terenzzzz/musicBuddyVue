0fe9a235cf1ce249b04d5abeae15dd9489d20a2b
Terenzzzz <terence592592@gmail.com>
2023-11-15 17:55:00 +0000

    change API address to 159.65.20.220:592

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,10 +2,13 @@
 import axios from 'axios'
 
 // 开发使用
-axios.defaults.baseURL = "http://0.0.0.0:592"
+// axios.defaults.baseURL = "http://0.0.0.0:592"
 
-
+// 阿里云服务器
 // axios.defaults.baseURL = "http://47.106.139.89:592"
+
+// TerenzDroplet
+axios.defaults.baseURL = "http://159.65.20.220:592"
 
 
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
