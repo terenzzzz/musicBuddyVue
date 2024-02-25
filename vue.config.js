fab8a14122dc8654bdd84d9e2c3e1b2ea254d8c8
Terenzzzz <terence592592@gmail.com>
2024-02-25 02:48:19 +0000

    fixed build

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,3 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
-
 module.exports = {
-  outputDir: 'dist',
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/terenzWebsite/'
-    : '/'
+  publicPath: './'
 }
