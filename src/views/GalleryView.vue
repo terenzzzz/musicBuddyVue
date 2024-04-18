bcc51f932b25e96633e537e2904e291f2884c368
Terenzzzz <terence592592@gmail.com>
2024-04-18 19:21:09 +0100

    gallery

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -1,6 +1,39 @@
 <template>
     <div>
-        <Waterfall :list="list" :width="250">
+
+        <div class="row m-0">
+            <div class="col-12 justify-content-center row m-0"> 
+                <div class="col-6 col-md-4 col-lg-3 col-xl-2 border-end row text-center m-0 p-2 justify-content-center " v-on:click="toggleView">
+                    <div class="col-3 my-auto">
+                        <img src="../assets/logo/kitty.png" class="img-fluid" > 
+                    </div>
+                    
+                    <p class="col-3 my-auto p-0 gochi-hand-regular fs-1">Kitty</p> 
+                </div>
+
+                <div class="col-6 col-md-4 col-lg-3 col-xl-2 row text-center m-0 p-2 justify-content-center " v-on:click="toggleView">
+                    <div class="col-3 my-auto">
+                        <img src="../assets/logo/cocktail.png" class="img-fluid" > 
+                    </div>
+                    
+                    <p class="col-3 my-auto p-0 gochi-hand-regular fs-1">Cocktail</p> 
+                </div>
+            </div>
+        </div>
+
+
+        <Waterfall :list="kittyList" :width="250" v-show="showKitty">
+            <template #item="{item, url, index}">
+                <div class="card">
+                    <LazyImg :url="url" class="card-img-top"/>
+                    <div class="card-footer text-body-secondary text-center gochi-hand-regular">
+                        {{ item.date }}
+                    </div>
+                </div>
+            </template>
+        </Waterfall>
+
+        <Waterfall :list="cockTailList" :width="250" v-show="!showKitty">
             <template #item="{item, url, index}">
                 <div class="card">
                     <LazyImg :url="url" class="card-img-top"/>
@@ -24,14 +57,22 @@ export default {
         Waterfall,LazyImg
     },
 
+    methods: {
+        toggleView(){
+            this.showKitty == true ? this.showKitty = false : this.showKitty = true
+        }
+    },
+
     data() {
         return {
-            list: []
+            showKitty: true,
+            kittyList: [],
+            cockTailList: []
         };
     },
     created() {
         //使用require.context动态导入图片
-        const files = require.context('@/assets/kitty', false, /\.jpg$/); // 这里假设你的图片格式为png，可以根据实际情况修改正则表达式
+        const files = require.context('@/assets/kitty', false, /\.jpg$/); 
         // 使用正则表达式匹配文件名中的日期部分，并将其转换为指定格式的日期字符串
         const dateRegex = /(\d{2})(\d{2})(\d{2})\.jpg/;
         files.keys().forEach(key => {
@@ -43,9 +84,28 @@ export default {
             const dateString = `${year}-${month}-${day}`;
             
             // 添加到list数组中
-            this.list.push({
+            this.kittyList.push({
                 date: dateString,
                 src: files(key)
+            });
+            }
+        });
+
+        //使用require.context动态导入图片
+        const cocktailFiles = require.context('@/assets/cocktail', false, /\.jpg$/); 
+        // 使用正则表达式匹配文件名中的日期部分，并将其转换为指定格式的日期字符串
+        cocktailFiles.keys().forEach(key => {
+            const fileName = key.match(dateRegex);
+            if (fileName) {
+            const year = '20' + fileName[1];
+            const month = fileName[2];
+            const day = fileName[3];
+            const dateString = `${year}-${month}-${day}`;
+            
+            // 添加到list数组中
+            this.cockTailList.push({
+                date: dateString,
+                src: cocktailFiles(key)
             });
             }
         });
