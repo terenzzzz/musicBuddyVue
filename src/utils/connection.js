a9eaa53c85f31bf064c6e631a48800f9ee2a2f61
Terenzzzz <terence592592@gmail.com>
2024-06-14 01:26:25 +0100

    back to development setting

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,3 +1,9 @@
 
-export const API_URL = 'https://terenzzzz.cn:6907'
+// Deploy
+// export const API_URL = 'https://terenzzzz.cn:6907'
+
+// Development
+export const API_URL = 'http://0.0.0.0:6906'
+
+
 export const SPOTIFY_AUTH_URL = `${API_URL}/api/spotifyLogin`
